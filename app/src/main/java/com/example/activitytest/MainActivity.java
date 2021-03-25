d90ab63ab7f93c312da692736b01532af928cb07
nn98 <jkllhgb@gmail.com>
2021-03-25 23:49:28 +0900

    Const layout Param Test

--- app/src/main/java/com/example/activitytest/MainActivity.java
+++ app/src/main/java/com/example/activitytest/MainActivity.java
@@ -12,8 +12,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout layout,l0,l1,l2;
-    ConstraintLayout.LayoutParams lp,lp0,lp1;
-    Button btn0;
+    ConstraintLayout.LayoutParams lp,lp0,lp1,lp2,lp3;
+    Button btns[]=new Button[20];
     EditText et0;
     TextView tv0;
     @Override
@@ -40,21 +40,35 @@ public class MainActivity extends AppCompatActivity {
         lp1.leftToLeft=ConstraintLayout.LayoutParams.PARENT_ID;
         lp1.rightToRight=ConstraintLayout.LayoutParams.PARENT_ID;
 
-        l1=new ConstraintLayout(this);
-        l1.setLayoutParams(lp1);
-        layout.addView(l1);
+//        l1=new ConstraintLayout(this);
+//        l1.setLayoutParams(lp1);
+//        layout.addView(l1);
 
+        btns[0]=new Button(this);
+        btns[0].setLayoutParams(lp);
+        btns[0].setText("Add");
+        l0.addView(btns[0]);
 
-        btn0=new Button(this);
-        btn0.setLayoutParams(lp1);
-        btn0.setText("Add");
-        l0.addView(btn0);
+        lp2=new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        lp2.rightToRight=ConstraintLayout.LayoutParams.PARENT_ID;
+        btns[1]=new Button(this);
+        btns[1].setLayoutParams(lp2);
+        btns[1].setText("Add");
+        l0.addView(btns[1]);
 
-        et0=new EditText(this);
-        et0.setLayoutParams(lp);
-        tv0=new TextView(this);
-        tv0.setLayoutParams(lp);
-        l1.addView(et0);
-        l1.addView(tv0);
+        /*
+        lp 속성이 변하면 해당 lp 가진 모든 뷰의 속성이 변동.
+        뷰마다 설정하는게 제일 베스트?
+        결국엔 const 잡아주려면
+        M M / W W / M W / W M 4개만 깔고 나머지는 다 람다식으로로
+        */
+
+//        et0=new EditText(this);
+//        et0.setLayoutParams(lp);
+//        tv0=new TextView(this);
+//        tv0.setLayoutParams(lp);
+//        l1.addView(et0);
+//        l1.addView(tv0);
     }
 }
